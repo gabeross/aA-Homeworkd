84e59c6278b884a004bf8e182a4e2c08bba8b74a
gabeross <gabeaross79@gmail.com>
2018-11-15 10:00:49 -0800

    Fix mailer and bcrypt gem; gem problem was issue with x-code

--- W4D4/solution/app/controllers/users_controller.rb
+++ W4D4/solution/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     if @user.save
       login_user!(@user)
       redirect_to cats_url
-      msg = UserMailer.welcome_email(u)
+      msg = UserMailer.welcome_email(@user)
       msg.deliver_now
     else
       flash.now[:errors] = @user.errors.full_messages
--- W4D4/solution/app/mailers/user_mailer.rb
+++ W4D4/solution/app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: 'from@ninetyninecats.com'
 
   def welcome_email(user)
     @user = user
     @url  = 'http://ninetyninecats.com/cats'
-    mail(to: user.email, subject: 'Welcome to Ninety-9 Cats! Rent your cat TODAY!')
+    mail(to: @user.username, subject: 'Welcome to Ninety-9 Cats! Rent your cat TODAY!')
   end
 end
